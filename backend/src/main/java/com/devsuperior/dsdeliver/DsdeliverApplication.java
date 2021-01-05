62db0d25ad6de448a07aff050095a725e3df49e7
Henrique Tavares Aguiar <ricktavares76@gmail.com>
2021-01-05 16:50:27 -0300

    ...

--- backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/DsdeliverApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DsdeliverApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(DsdeliverApplication.class, args);
 	}
